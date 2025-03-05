3c9c29f168982caca51162a933f73183f5a0e239
yushna <buddhacharyay@gmail.com>
2025-03-05 21:57:48 +0545

    side header build

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,90 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
+import {
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  CodeOutlined,
+  HomeOutlined,
+  UserOutlined,
+  MehOutlined
+} from '@ant-design/icons';
+import {Breadcrumb, Button, Layout, Menu, theme } from 'antd';
 
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-          <h1 className='font-black'>Hello what's going on</h1>
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+const { Header, Sider, Content } = Layout;
+function getItem(key, label, icon, children) {
+  return {
+    key,
+    label,
+    icon,
+    children,
+  };
 }
 
-export default App
+const items = [
+  getItem('1', 'Home', <HomeOutlined />),
+  getItem('sub1', 'About Me', <UserOutlined />, [
+    getItem('3', 'Education'),
+    getItem('4', 'Skills'),
+    getItem('5', 'Experience'),
+  ]),
+  getItem('6', 'Projects', <CodeOutlined />),
+];
+
+
+
+const App = () => {
+  const [collapsed, setCollapsed] = useState(false);
+  const {
+    token: { colorBgContainer, borderRadiusLG},
+  } = theme.useToken();
+  return (
+    <Layout className='bg-green-300'>
+      <Sider trigger={null} collapsible collapsed={collapsed}
+      className='w-60 h-[100vh] pt-5 sticky'>
+        <div className='px-5 h-12 text-white font-black text-2xl font-serif ' >
+          {collapsed?<MehOutlined/> : <><MehOutlined/> yushna417</> }</div>
+        <Menu
+          className='text-base font-normal  font-sans'
+          theme="dark"
+          mode="inline"
+          defaultSelectedKeys={['1']}
+          items={items}
+        />
+      </Sider>
+      <Layout>
+        <Header
+          style={{
+            padding: 0,
+            background: colorBgContainer,
+          }}
+          className='bg-green-400 w-full border'
+        >
+          <Button
+            type="text"
+            icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+            onClick={() => setCollapsed(!collapsed)}
+            style={{
+              fontSize: '23px',
+              width: 64,
+              height: 64,
+            }}
+           
+          />
+        </Header>
+        <Content
+        className='bg-green-300'
+         style={{
+          margin: '24px 16px',
+          padding: 24,
+          minHeight: 280,
+          background: colorBgContainer,
+          borderRadius: borderRadiusLG,
+        }}
+        >
+          Content
+        </Content>
+      </Layout>
+    </Layout>
+  );
+};
+export default App;
