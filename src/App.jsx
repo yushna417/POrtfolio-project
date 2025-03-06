f4f6f84fe138cad962b26a03bf30af3e34afef95
yushna <buddhacharyay@gmail.com>
2025-03-06 11:48:24 +0545

    default layout fixed

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,8 @@ import {
 } from '@ant-design/icons';
 import { Button, Layout, Menu, theme, ConfigProvider } from 'antd';
 
-
 const { Header, Sider, Content } = Layout;
+
 function getItem(key, label, icon, children) {
   return {
     key,
@@ -30,13 +30,12 @@ const items = [
   getItem('6', 'Projects', <CodeOutlined />),
 ];
 
-
-
 const App = () => {
   const [collapsed, setCollapsed] = useState(false);
   const {
-    token: { colorBgContainer, bodyBg},
+    token: { colorBgContainer },
   } = theme.useToken();
+  
   return (
     <ConfigProvider
       theme={{
@@ -45,57 +44,70 @@ const App = () => {
           colorPrimary: '#27667B',
           borderRadius: 2,
           bodyBg: '#C7D9DD',
-          
+
           // Alias Token
           colorBgContainer: '#ADB2D4',
         },
       }}
     >
-    
-    <Layout >
-      <Sider trigger={null} collapsible collapsed={collapsed}
-      className='w-60 h-[100vh] pt-5 sticky'>
-        <div className='px-5 h-12 text-white font-black text-xl montserrat-custom  tracking-wider' >
-          {collapsed?<MehOutlined/> : <><MehOutlined/> PortFolio</> }</div>
-        <Menu
-          className='text-base font-normal montserrat-custom  '
-          theme="dark"
-          mode="inline"
-          defaultSelectedKeys={['1']}
-          items={items}
-        />
-      </Sider>
-      <Layout>
-        <Header
-          style={{
-            padding: 0,
-            background: colorBgContainer,
-          }}
-          className=' w-[83vw] border   shadow-2xl '
+      <Layout style={{ minHeight: '100vh', minWidth: '100vw' }}>
+        <Sider 
           
+          trigger={null} 
+          collapsible 
+          collapsed={collapsed} 
+          width={250} // Set fixed width for Sider
+          className="h-[100vh] pt-2 sticky"
         >
-          <Button
-            type="text"
-            icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
-            onClick={() => setCollapsed(!collapsed)}
-            style={{
-              fontSize: '23px',
-              width: 64,
-              height: 64,
-            }}
-            className='text-white'
-           
+          <div
+            className={`px-5 h-12 text-white font-black tracking-wider flex items-center justify-center ${
+              collapsed ? 'text-3xl flex items-center justify-center' : 'text-xl'
+            } montserrat-custom`}
+          >
+            {collapsed ? <MehOutlined /> : <><MehOutlined /> PortFolio</>}
+          </div>
+          <Menu
+            className="text-base font-normal montserrat-custom"
+            theme="dark"
+            mode="inline"
+            defaultSelectedKeys={['1']}
+            items={items}
           />
-        </Header>
-        <Content
-        className='bg-[#F7F7F7] indie-flower-regular mt-[9px] mx-5 max-w-5xl w-[80vw] h-full border-2 rounded-t-lg shadow-2xl p-5'
-         
-        >
-          Content, Hi, this is going to be the content of the page
-        </Content>
+        </Sider>
+        <Layout style={{ minWidth: 'auto'}}>
+          <Header
+            style={{
+              padding: 0,
+              background: colorBgContainer,
+            }}
+            className="w-full shadow-2xl"
+          >
+            <Button
+              type="text"
+              icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+              onClick={() => setCollapsed(!collapsed)}
+              style={{
+                fontSize: '23px',
+                width: 64,
+                height: 64,
+              }}
+              className="text-white"
+            />
+          </Header>
+          <Content
+            className="bg-[#F7F7F7] indie-flower-regular mt-[9px] mx-5 rounded-t-lg shadow-2xl p-5"
+            style={{
+              marginInline: collapsed ? 40 : 25, // Adjust content margin based on Sider collapse state
+              transition: 'margin-left 0.3s', // Smooth transition for margin adjustment
+              padding: '20px',
+            }}
+          >
+            Content, Hi, this is going to be the content of the page
+          </Content>
+        </Layout>
       </Layout>
-    </Layout>
     </ConfigProvider>
   );
 };
+
 export default App;
